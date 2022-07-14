02fa57cd668f12233ce1de4c5510199124718f67
Troy <wilson.troy.anthony@gmail.com>
2022-07-13 20:13:45 -0400

    Add project files.

---
+++ Basic String Processing 2/BasicStringProcessing.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+
+namespace Basic_String_Processing_2
+{
+    internal class BasicStringProcessing
+    {
+        List<string> words = new List<string>();
+        public BasicStringProcessing()
+        {
+            do
+            {
+                GetWord(); 
+            } while(Continue());
+
+            Clear();
+
+            foreach(var item in words)
+            {
+                Write(item + " ");
+            }
+            WriteLine("\nPress any key to continue.");
+            ReadKey();
+        }
+
+
+        private void GetWord()
+        {
+            Write("Enter some text: ");
+            words.Add(ReadLine());
+        }
+
+        private static string[] yes = { "yes", "y", "yup" };
+        private static string[] no = { "no", "n", "nope" };
+
+        private bool Continue()
+        {
+            do
+            {
+                Write("Would you like to continue Y/N?");
+                string input = ReadLine().ToLower().Trim();
+                if(yes.Contains(input)) return true;
+                else if(no.Contains(input)) return false; 
+            } while(true);
+        }
+    }
+}
---
+++ Basic String Processing 2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Basic_String_Processing_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            do
+            {
+                _ = new BasicStringProcessing();
+            } while(false);
+        }
+    }
+}
